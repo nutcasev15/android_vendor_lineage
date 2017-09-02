98d85f952496726092d58a88b8ab0acf6a8f2406
jrior001 <jriordan001@gmail.com>
2017-09-02 18:52:56 -0400

    soong_config: Add BOARD_USES_PRE_ION_X86
    * Soong product variable 'Pre_ion_x86'
    
    Change-Id: I6c55cd74ec0a1f43b7343427d7351561f53611ca

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,8 +3,12 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+	Pre_ion_x86 struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
+	Pre_ion_x86  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,6 +4,7 @@ lineage_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Lineage": {'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "Pre_ion_x86": $(if $(filter true,$(BOARD_USES_PRE_ION_X86)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
