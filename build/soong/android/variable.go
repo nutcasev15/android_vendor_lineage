815bc59a33d164726b46884541d809c9a676a90e
nutcasev15 <tab.andsupp@gmail.com>
2019-07-15 14:14:33 +0530

    soong_config: Z00A/8: Allow Both Cflags & Cppflags to be Defined

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -2,6 +2,7 @@ package android
 type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
+		Cppflags []string
 	}
 	Needs_legacy_camera_hal1_dyn_native_handle struct {
 		Cppflags []string
@@ -27,6 +28,7 @@ type Product_variables struct {
 	}
 	Pre_ion_x86 struct {
 		Cflags []string
+		Cppflags []string
 	}
 }
 
